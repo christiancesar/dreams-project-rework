17190e0aeefa851a627ac3302f3408e2eed2bee7
Christian Cesar <christiancnp@gmail.com>
2022-05-31 22:56:40 -0400

    feat(apiGateway): create comunication service with hotel microservice

---
+++ apiGateway/src/services/HotelService.ts
@@ -0,0 +1,6 @@
+import { ChannelCredentials } from '@grpc/grpc-js';
+import { HotelsClient } from '../protos/hotel/hotel_grpc_pb';
+
+const hotelClient = new HotelsClient('0.0.0.0:50053', ChannelCredentials.createInsecure()) 
+
+export default hotelClient;
